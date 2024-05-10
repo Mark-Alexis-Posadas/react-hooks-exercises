c501a0020b09a727a63fa418d5dc8b8f9926f026
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-05-11 05:53:15 +0800

    done toggle switch exercise

--- src/UseState/ToggleSwitch.jsx
+++ src/UseState/ToggleSwitch.jsx
@@ -5,10 +5,24 @@ export default function ToggleSwitch() {
 
   return (
     <>
-      <button onClick={() => setContentVisible(!isContentVisible)}>
+      <button
+        onClick={() => setContentVisible(!isContentVisible)}
+        className={`rounde p-2 ${
+          isContentVisible
+            ? "bg-green-300 text-white"
+            : "bg-slate-300 text-black"
+        }`}
+      >
         {isContentVisible ? "Off" : "On"}
       </button>
-      {isContentVisible && <p>content</p>}
+      {isContentVisible && (
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+          molestiae numquam soluta saepe modi, qui laboriosam? Pariatur
+          quibusdam earum repudiandae ad. Accusamus, ipsa veniam alias tempora
+          dignissimos nihil officia maiores.
+        </p>
+      )}
     </>
   );
 }
