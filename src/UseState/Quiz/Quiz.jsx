4145cc8f2173aba910a5f87f29b240f3e1bc1f76
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-05-12 20:01:50 +0800

    added states

--- src/UseState/Quiz/Quiz.jsx
+++ src/UseState/Quiz/Quiz.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import Question from "./Question";
 import Score from "./Score";
 import quizQuestions from "./data";
 export default function Quiz() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [score, setScore] = useState(0);
+  const [quizEnd, setQuizEnd] = useState(false);
   return (
     <div>
       <h1 className="font-bold text-xl">Quiz App</h1>
