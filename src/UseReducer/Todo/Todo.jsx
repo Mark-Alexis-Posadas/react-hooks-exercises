e0ff83b4eb57e2e29bcf8dd93abf3d799177f4b7
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-09-12 17:06:39 +0800

    done todo reducer

--- src/UseReducer/Todo/Todo.jsx
+++ src/UseReducer/Todo/Todo.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useEffect } from "react";
 import TodoItem from "./TodoItem";
 
 const initialState = {
@@ -21,6 +21,14 @@ const reducer = (state, action) => {
         ...state,
         todos: state.todos.filter((_, index) => index !== action.payload),
       };
+    case "EDIT_TODO":
+      const { idx, newText } = action.payload;
+      return {
+        ...state,
+        todos: state.todos.map((item, i) =>
+          i === idx ? { ...item, text: newText } : item
+        ),
+      };
     case "TOGGLE_TODO":
       return {
         ...state,
@@ -35,8 +43,21 @@ const reducer = (state, action) => {
   }
 };
 
+const loadStateFromLocalStorage = () => {
+  const savedState = localStorage.getItem("todoAppState");
+  return savedState ? JSON.parse(savedState) : initialState;
+};
+
+const saveStateToLocalStorage = (state) => {
+  localStorage.setItem("todoAppState", JSON.stringify(state));
+};
+
 export default function Todo() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, loadStateFromLocalStorage());
+
+  useEffect(() => {
+    saveStateToLocalStorage(state);
+  }, [state]);
 
   const handleAddTodo = () => {
     if (state.inputValue.trim() !== "") {
@@ -46,12 +67,17 @@ export default function Todo() {
 
   const handleDeleteTodo = (index) => {
     dispatch({ type: "DELETE_TODO", payload: index });
-    console.log(index);
+  };
+
+  const handleEditTodo = (index, newText) => {
+    dispatch({
+      type: "EDIT_TODO",
+      payload: { idx: index, newText },
+    });
   };
 
   const handleToggleTodo = (index) => {
     dispatch({ type: "TOGGLE_TODO", payload: index });
-    console.log(index);
   };
 
   return (
@@ -79,6 +105,7 @@ export default function Todo() {
             item={item}
             handleToggleTodo={handleToggleTodo}
             handleDeleteTodo={handleDeleteTodo}
+            handleEditTodo={handleEditTodo}
           />
         ))}
       </ul>
--- src/UseReducer/Todo/TodoItem.jsx
+++ src/UseReducer/Todo/TodoItem.jsx
@@ -1,26 +1,64 @@
+import React, { useState } from "react";
+
 export default function TodoItem({
   item,
   index,
   handleToggleTodo,
   handleDeleteTodo,
+  handleEditTodo,
 }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editText, setEditText] = useState(item.text);
+
+  const handleSaveEdit = () => {
+    handleEditTodo(index, editText);
+    setIsEditing(false);
+  };
+
   return (
     <li className="flex items-center justify-between p-2 rounded bg-white shadow-md my-2">
-      <span
-        onClick={() => handleToggleTodo(index)}
-        className={item.completed ? "line-through" : ""}
-      >
-        {" "}
-        {item.text}
-      </span>
+      {isEditing ? (
+        <div className="flex gap-2">
+          <input
+            className="p-2 rounded border"
+            type="text"
+            value={editText}
+            onChange={(e) => setEditText(e.target.value)}
+          />
+          <button
+            className="text-white rounded p-1 bg-green-500"
+            onClick={handleSaveEdit}
+          >
+            Save
+          </button>
+          <button
+            className="text-white rounded p-1 bg-gray-500"
+            onClick={() => setIsEditing(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      ) : (
+        <span
+          onClick={() => handleToggleTodo(index)}
+          className={item.completed ? "line-through" : ""}
+        >
+          {item.text}
+        </span>
+      )}
 
       <div className="flex items-center gap-3">
-        <button className="text-white rounded p-1 bg-blue-500">edit</button>
+        <button
+          className="text-white rounded p-1 bg-blue-500"
+          onClick={() => setIsEditing(!isEditing)}
+        >
+          {isEditing ? "Cancel" : "Edit"}
+        </button>
         <button
           className="text-white rounded p-1 bg-red-500"
           onClick={() => handleDeleteTodo(index)}
         >
-          delete
+          Delete
         </button>
       </div>
     </li>
